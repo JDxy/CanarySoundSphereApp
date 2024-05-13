9ea7d16309d56553504184b64efcf87f9cd2f82e
AIPONCEP <ana_ponce_perez@hotmail.es>
2024-05-13 23:16:57 +0100

    update client add endpoint for author

--- app/src/main/java/com/project/canary_sound_sphere_app/data/CanarySoundSphereClient.kt
+++ app/src/main/java/com/project/canary_sound_sphere_app/data/CanarySoundSphereClient.kt
@@ -4,7 +4,9 @@ import com.project.canary_sound_sphere_app.model.EventModel
 import com.project.canary_sound_sphere_app.model.EventModelDetails
 import com.project.canary_sound_sphere_app.model.ResultApi
 import com.project.canary_sound_sphere_app.util.Constants.Companion.ENDPOINT_EVENT_BY_ID
+import com.project.canary_sound_sphere_app.util.Constants.Companion.ENDPOINT_GET_ALL_AUTHORS
 import com.project.canary_sound_sphere_app.util.Constants.Companion.ENDPOINT_GET_ALL_EVENTS
+import com.project.canary_sound_sphere_app.util.Constants.Companion.ENDPOINT_GET_AUTHOR_BY_ID
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -17,11 +19,13 @@ interface CanarySoundSphereClient {
     suspend fun getListEvents(): Response<List<EventModel>>
     @GET(ENDPOINT_EVENT_BY_ID)
     suspend fun getDetailsEvent(@Path("id") id: String): Response<EventModelDetails>
-    /*
+/*
     @GET(ENDPOINT_GET_ALL_AUTHORS)
-    suspend fun getListAuthors(): Response<ResultApi>
+    suspend fun getListAuthors():  Response<List<AuthorModel>>
 
     @GET(ENDPOINT_GET_AUTHOR_BY_ID)
-    suspend fun getDetailsAuthor(@Path("id") id: Int): Response<PokeModelDetails>
-    */
+    suspend fun getDetailsAuthor(@Path("id") id: String): Response<AuthorModelDetails>
+    
+ */
+
 }
